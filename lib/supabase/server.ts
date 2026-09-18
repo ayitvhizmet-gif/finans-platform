4b2d8c2cff470a4a289f316e3f8d929499489294
LordMacro <ayitvhizmet@gmail.com>
2026-09-18 23:12:24 +0300

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,8 +1,14 @@
-import { createServerClient } from "@supabase/ssr";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export async function createClient() {
-  const cookieStore = await cookies();
+type CookieToSet = {
+  name: string;
+  value: string;
+  options: CookieOptions;
+};
+
+export function createClient() {
+  const cookieStore = cookies();
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -11,7 +17,7 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: CookieToSet[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
